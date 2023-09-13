173867fd15877f2e2abd4a6c2b26340cf9ba792e
Sanghyun124 <cloush24@gmail.com>
2023-09-13 13:02:26 +0900

    230913 daily

---
+++ 23.09/230912/BJ_6064.py
@@ -0,0 +1,13 @@
+t=int(input())
+
+for _ in range(t):
+    m,n,x,y=map(int,input().split())
+    f=1
+    while(x<=m*n):
+        if x%n == y%n:
+            print(x)
+            f=0
+            break
+        x+=m
+    if f:
+        print(-1)
